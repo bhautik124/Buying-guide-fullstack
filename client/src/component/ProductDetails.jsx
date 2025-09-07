1821cc103042fdc75e39ddbbf2d840faab73a934
Bhautik Sangani <161152146+bhautik124@users.noreply.github.com>
2025-09-07 11:51:22 +0530

    Update ProductDetails.jsx

--- client/src/component/ProductDetails.jsx
+++ client/src/component/ProductDetails.jsx
@@ -225,7 +225,7 @@ const ProductDetails = () => {
     const fetchReviews = async () => {
       try {
         const response = await axios.get(
-          `http://https://buying-guide-fullstack-backend.onrender.com/api/reviews/${productId}`
+          `https://buying-guide-fullstack-backend.onrender.com/api/reviews/${productId}`
         );
         setReviews(response.data);
       } catch (error) {
